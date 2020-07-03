5eab8bfac48940fe2749c9c9a47f91cb75531a3f
Aaron Batey <abatey@grapevine6.com>
2020-07-02 20:39:21 -0400

    add flexbox to reports

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
-var reportsWidget = {
+const reportsWidget = {
     options: {
         containerSelector: '.reports',
         template: (
             '{{#.}}' +
-                '<article class="reports_item">' +
+                '<article class="reports_item col-4 col-sm-6 col-xs-12">' +
                     '<a href="{{cover}}" target="_blank">' +
-                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>'
+                        '<img class="reports_cover" src="{{cover}}" alt="{{title}} Cover"/>' +
                     '</a>' +
                     '<footer class="reports_docs">' +
                         '{{#documents}}' +
                             '<h3 class="reports_title">' +
-                                '<a href="{{url}}" target="_blank">{{title}}</a>' +
+                                '<a href="{{url}}" target="_blank">{{title}} ' + 
+                                    '<span>({{file_size}} {{file_type}})' +
+                                    '</span>' +
+                                '</a>' +
                             '</h3>' +
                         '{{/documents}}' +
                     '</footer>' +
@@ -19,12 +22,12 @@ var reportsWidget = {
         )
     },
 
-    init: function() {
+    init: async function() {
         this.renderReports(reportData || []);
     },
 
-    renderReports: function(reports) {
-        var inst = this,
+    renderReports: async function(reports) {
+        const inst = this,
             options = inst.options;
 
         $(options.containerSelector).html(Mustache.render(options.template, reports));
